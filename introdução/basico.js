c039cf3119f7d7653882a631b8a6c6c81a2ff142
Batcleta <thewazig.wf@gmail.com>
2022-01-04 18:15:45 -0300

    first commit

---
+++ introdução/basico.js
@@ -0,0 +1,6 @@
+"use strict";
+let a;
+a = "teste de Ts atualizado bilola";
+console.log(a);
+// Using live-server
+// tsc -w watch file changes ans recompile a new js
---
+++ introdução/basico.ts
@@ -0,0 +1,6 @@
+let a:string
+a = "teste de Ts atualizado bilola"
+console.log(a)
+
+// Using live-server
+// tsc -w watch file changes ans recompile a new js
